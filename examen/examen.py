63d3f63a83111329819b7231972a753f0f039764
203437-A <203437@ids.upchiapas.edu.mx>
2022-09-30 10:37:43 -0500

    change exam

--- examen/examen.py
+++ examen/examen.py
@@ -7,18 +7,18 @@ def comer(idPersona):
     palillo = 2
     print()
     print(f"La persona {idPersona} tiene {palillo} palillos y empezó a comer")
-    time.sleep(1)
+    time.sleep(3)
     print(f"La persona {idPersona} dejo el palillo"'\n'f"La persona {idPersona} dejo de comer")
-    time.sleep(1)
+    time.sleep(3)
 
 class Persona(threading.Thread):
-    def __init__(self, persona):
+    def __init__(self, idPersona):
         threading.Thread.__init__(self)
-        self.persona = persona
+        self.idPersona = idPersona
 
     def run(self):
         mutex.acquire()
-        comer(self.persona)
+        comer(self.idPersona)
         mutex.release()
 
 personas = [Persona(1), Persona(2), Persona(3), Persona(4), Persona(5), Persona(6), Persona(7), Persona(8)]
